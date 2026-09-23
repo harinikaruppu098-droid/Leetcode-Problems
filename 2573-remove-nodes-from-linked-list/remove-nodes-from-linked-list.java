655cb1d0bf4a1b85add30be290325a972b92451f
HARINISRI KARUPPUSAMY <harinikaruppu098@gmail.com>
2026-09-23 20:45:32 +0530

    Time: 6 ms (92.76%) | Memory: 130.5 MB (69.49%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -0,0 +1,33 @@
+class Solution {
+    public ListNode removeNodes(ListNode head) {
+        head = reverse(head);
+
+        int max = head.val;
+        ListNode curr = head;
+
+        while (curr.next != null) {
+            if (curr.next.val < max) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+                max = curr.val;
+            }
+        }
+
+        return reverse(head);
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}
